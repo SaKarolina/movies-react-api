fc5013f17de79a5b8497827b05133ed0cbf6fac7
SaKarolina <salnaite.k@gmail.com>
2022-08-09 17:54:36 +0300

    Trending list

---
+++ src/Components/Card.jsx
@@ -0,0 +1,32 @@
+import "./card.scss";
+import star from '../Images/star.svg';
+
+const Card = ({
+    id,
+    poster,
+    title,
+    date,
+    media_type,
+    vote_average,
+    votes
+}) => {
+    return (
+        <li className="item-row" id={id}>
+            {poster ? (<img className='img' src={`https://image.tmdb.org/t/p/w200${poster}`} alt="movie_img"></img>) : (<div className="filler-poster"></div>)}
+            <div className='info'>
+                <span style={{ fontWeight: '900', textAlign: 'center' }}>{title}</span>
+                <span style={{ marginTop: "20px" }}>{date ? date.substring(0, 4) : '-'}</span>
+                <div className="vote">
+                    <img className='starImg' style={{ width: '20px', heigth: '20px' }} src={star} alt='star'></img>
+                    <div>
+                        <p style={{ fontWeight: '600'}}>{(vote_average || 0).toFixed(1)}/10</p>
+                        <p style={{ fontSize: '10px', color: '#a4a4a4' }}>{votes} votes</p>
+                    </div>
+                </div>
+                <span className="type" style={{ fontSize: '15px', color: '#a4a4a4', marginTop: "15px"  }}>{media_type === "tv" ? `${media_type} show` : media_type}</span>
+            </div>
+        </li>
+    );
+};
+
+export default Card;
--- src/Components/Header/Trending.jsx
+++ src/Components/Header/Trending.jsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from "react";
+import Card from "../Card";
+import '../../app.scss';
+
 
 function Trending() {
-  
+
+  const [weekTrends, setWeekTrends] = useState([]);
+
+  const getTrending = () => {
+    fetch('https://api.themoviedb.org/3/trending/all/week?api_key=9207b7bb9ad666f628ccc02d8fdd966e')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const dataCopy = data.results;
+        if (dataCopy !== null) {
+          setWeekTrends(dataCopy);
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getTrending();
+  }, []);
+
 
   return (
     <div>
-    
+      <h1 className="pageTitle">Trending Today</h1>
+      <ul className="front-list-container">
+        {weekTrends &&
+          weekTrends.map((movie) => (
+            <Card
+              key={movie.id}
+              id={movie.id}
+              poster={movie.poster_path}
+              title={movie.title || movie.name}
+              date={movie.first_air_date || movie.release_date}
+              media_type={movie.media_type}
+              vote_average={movie.vote_average}
+              votes={movie.vote_count}
+            />
+          ))}
+      </ul>
     </div>
   )
 }
